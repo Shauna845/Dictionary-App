369a0e0ea68233d16bbfdea5de7d4a7262491ba4
Shauna845 <shauna10@hotmail.co.uk>
2024-01-12 01:24:47 +0000

    Recreated app

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.jpeg";
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small>
+            Coded by Shauna Bratten open-sourced on{" "}
+            <a
+              href="https://github.com/Shauna845/React-Dictionary-Project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://sb-react-dictionary-app.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = "97a2aef61tb590co334c6f1f08042a43";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
+}
